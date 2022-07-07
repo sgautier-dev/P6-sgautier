2cb7d497c5d300fbdcbfeb31dad5fff6bae08fe4
sebdina <95606603+sebdina@users.noreply.github.com>
2022-07-07 09:12:51 +0400

    in UserCtrl signup & login funct refactored to async and gensalt used

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,41 +2,50 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
+exports.signup = async (req, res, next) => {
+    try {
+        //generate salt against possible rainbow table attacks 
+        const salt = await bcrypt.genSalt(10);
+        // Hash&Salt password
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        console.log(hashedPassword);
+        try {
             const user = new User({
                 email: req.body.email,
-                password: hash
+                password: hashedPassword
             });
-            user.save()
-                .then(() => res.status(201).json({ message: 'User created !' }))
-                .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+            await user.save();
+            res.status(201).json({ message: 'User created !' })
+        } catch (error) {
+            res.status(400).json({ error })
+        }
+
+    } catch (error) {
+        error => res.status(500).json({ error });
+    }
 };
 
-exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
- };
+exports.login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(401).json({ message: 'Incorrect login/password' });
+        }
+        const valid = await bcrypt.compare(req.body.password, user.password);
+        if (!valid) {
+            return res.status(401).json({ message: 'Incorrect login/password' });
+        }
+        res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+                { userId: user._id },
+                'RANDOM_TOKEN_SECRET',
+                { expiresIn: '24h' }
+            )
+        });
+
+    } catch (error) {
+        error => res.status(500).json({ error });
+    }
+
+};
