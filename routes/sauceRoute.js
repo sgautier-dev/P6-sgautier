5cc3f6ee91d498697194d08ce9331d19726237a8
sebdina <95606603+sebdina@users.noreply.github.com>
2022-07-12 20:18:23 +0200

    like function started

--- routes/sauceRoute.js
+++ routes/sauceRoute.js
@@ -10,5 +10,6 @@ router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.setLikeSauce);
 
 module.exports = router;
