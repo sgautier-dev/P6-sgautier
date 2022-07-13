fcaa17d0366dd5a31740a888de08c4e87533451c
sebdina <95606603+sebdina@users.noreply.github.com>
2022-07-13 17:34:58 +0200

    like and dislike set

--- controllers/sauceCtrl.js
+++ controllers/sauceCtrl.js
@@ -79,34 +79,30 @@ exports.getAllsauce = async (req, res, next) => {
         const sauces = await Sauce.find();
         res.status(200).json(sauces);
     } catch (error) {
-        res.status(400).json({ error: error });
+        res.status(400).json({ error });
     }
 };
 
 exports.setLikeSauce = async (req, res, next) => {
     try {
         const sauce = await Sauce.findOne({ _id: req.params.id });
-        console.log(sauce);
+
         if (sauce.userId != req.auth.userId || req.body.userId != req.auth.userId) {
             res.status(403).json({ message: 'Not authorized' });
         } else {
             const matchLiked = sauce.usersLiked.find(userLiked => {
                 return userLiked === req.body.userId;
             });
-            console.log(matchLiked);
             const matchDisliked = sauce.usersDisliked.find(userDisliked => {
                 return userDisliked === req.body.userId;
             });
-            console.log(matchDisliked);
-            console.log(req.body.like);
+            let message = '';
             switch (req.body.like) {
                 case 1:
                     if (!matchLiked) {
                         sauce.likes++;
                         sauce.usersLiked.push(req.body.userId);
-                        console.log(sauce.likes);
-                        console.log(sauce.usersLiked);
-                        const message = 'Like added!';
+                        message = 'Like added!';
                     }
                     break;
                 case 0:
@@ -115,35 +111,31 @@ exports.setLikeSauce = async (req, res, next) => {
                         sauce.usersLiked = sauce.usersLiked.filter(userLiked => {
                             return userLiked !== req.body.userId;
                         });
-                        const message = 'Like removed!';
+                        message = 'Like removed!';
                     }
                     if (matchDisliked) {
                         sauce.dislikes--;
                         sauce.usersDisliked = sauce.usersDisliked.filter(userDisliked => {
                             return userDisliked !== req.body.userId;
                         });
-                        const message = 'Dislike removed!';
+                        message = 'Dislike removed!';
                     }
                     break;
                 case -1:
                     if (!matchDisliked) {
                         sauce.dislikes++;
                         sauce.usersDisliked.push(req.body.userId);
-                        console.log(sauce.dislikes);
-                        console.log(sauce.usersDisliked);
-                        const message = 'Dislike added!';
+                        message = 'Dislike added!';
                     }
                     break;
                 default:
                     throw Error('Unvalid Like number');
             }
             try {
-                console.log(sauce);
-                //await Sauce.updateOne({ _id: req.params.id }, { ...sauce, _id: req.params.id });
                 await sauce.save();
                 res.status(200).json({ message: message });
             } catch (error) {
-                res.status(401).json({ error })
+                res.status(400).json({ error })
             }
         }
     } catch (error) {
