837d2f05e8cb110ce9db9198656c1b26ac01845f
sebdina <95606603+sebdina@users.noreply.github.com>
2022-07-27 15:21:05 +0200

    added express rate limit

--- app.js
+++ app.js
@@ -35,6 +35,15 @@ app.use(mongoSanitize());
 const xss = require('xss-clean');
 app.use(xss());
 
+//Add middleware to limit repeated requests
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
 
 // Mounting userRouter on '/api/auth' path.
 const userRoutes = require('./routes/userRoute');
